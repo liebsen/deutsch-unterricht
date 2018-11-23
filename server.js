486f3b69d96a129581121c3d419b92baa2230975
martin <telemagico@gmail.com>
2018-11-23 15:44:59 -0300

    start by saving things

--- server.js
+++ server.js
@@ -1,29 +1,60 @@
-  var Watcher  = require('feed-watcher'),
-      feed     = 'https://www.neues-deutschland.de/rss/neues-deutschland.xml',
-      interval = 30 // seconds
+var MongoClient = require('mongodb').MongoClient
+var assert = require('assert')
+var express = require('express')
+var app = express()
+var db = null
 
-  // if not interval is passed, 60s would be set as default interval.
-  var watcher = new Watcher(feed, interval)
+// watcher
 
-  // Check for new entries every n seconds.
-  watcher.on('new entries', function (entries) {
-    entries.forEach(function (entry) {
-      console.log(entry.title)
-    })
-  })
+var Watcher  = require('feed-watcher'),
+  feed     = 'https://www.neues-deutschland.de/rss/neues-deutschland.xml',
+  interval = 60 * 60 * 6 // hours
+
+// if not interval is passed, 60s would be set as default interval.
+var watcher = new Watcher(feed, interval)
+
+// Check for new entries every n seconds.
+watcher.on('new entries', (entries) => {
+  entries.forEach((entry) => {
+    try {
+      db.collection('news').updateOne({title:entry.title},{$set:entry},{upsert:true})
+    } catch (e) {
+      console.log(e);
+    }
+  })      
+})
+
+
+// routes
+MongoClient.connect(process.env.mongourl,(err,database) => {
+  assert.equal(err,null)
+  db = database.db('deutschunterricht')
+  //db = database.db('deutschunterricht')
 
   // Start watching the feed.
   watcher
     .start()
-    .then(function (entries) {
-      //console.log(entries)
-      entries.forEach(function (entry) {
-        console.log(entry.title)
+    .then((entries) => {
+      entries.forEach((entry) => {
+        try {
+          db.collection('news').updateOne({title:entry.title},{$set:entry},{upsert:true})
+        } catch (e) {
+          console.log(e);
+        }
       })      
     })
-    .catch(function(error) {
+    .catch((error) => {
       console.error(error)
-    })
+    })    
+})
 
-  // Stop watching the feed.
-  //watcher.stop()
+app.get('/', (req,res) => {
+  db.collection('news').find({}).toArray((err,doc) => {
+    assert.equal(err,null)
+    res.send(JSON.stringify(doc))
+  })
+})
+
+app.listen(3000,() => {
+  console.log('Listening on port 3000')
+})
